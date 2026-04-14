ee2a50fd0e5b5b7aca5e1ed1ab8d20e2f610ac96
PatrickKillebrew <killebrewpatrick@gmail.com>
2026-04-14 15:56:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -536,4 +536,4 @@ def handle_get_status(data):
     })
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
